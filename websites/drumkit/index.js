cc778d5ec82baedefeb2e74cc8454a124074e0fa
Staartline <113581885+Staartline@users.noreply.github.com>
2023-05-24 23:36:59 -0400

    Drumkit Project Template
    Added the Drumkit Template

---
+++ websites/drumkit/index.js
@@ -0,0 +1,6 @@
+function playSound(buttonpressed){
+	//checks for which button was pressed, or a class associated with the button first?
+	//plays sound at some letter(buttonpressed.)
+}
+
+document.querySelector("drum").addEventListener();
