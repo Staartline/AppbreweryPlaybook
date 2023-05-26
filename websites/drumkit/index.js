f59ac876164b3d17c4064eece634945691975213
Staartline <113581885+Staartline@users.noreply.github.com>
2023-05-26 19:36:17 -0400

    Work on Drumkit
    Added resources to resource page
    Added code to JS file

--- websites/drumkit/index.js
+++ websites/drumkit/index.js
@@ -1,6 +1,14 @@
-function playSound(buttonpressed){
+
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
+	document.querySelectorAll(".drum")[i].addEventListener("click", playSound);
+	console.log(i);
+}
+
+
+function playSound(){
+	alert("clicked");
+
 	//checks for which button was pressed, or a class associated with the button first?
 	//plays sound at some letter(buttonpressed.)
 }
 
-document.querySelector("drum").addEventListener();
