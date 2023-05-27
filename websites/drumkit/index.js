cff1f9046a8f9c891dba7616b19ff8df69b27a15
Staartline <113581885+Staartline@users.noreply.github.com>
2023-05-26 20:22:09 -0400

    drumkit Update
    Added Sounds to JS file in drumkit
    Added a Calculator JS file (No html yet)

--- websites/drumkit/index.js
+++ websites/drumkit/index.js
@@ -1,14 +1,21 @@
 
 for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
 	document.querySelectorAll(".drum")[i].addEventListener("click", playSound);
-	console.log(i);
 }
 
 
 function playSound(){
-	alert("clicked");
+	crash.play();
 
 	//checks for which button was pressed, or a class associated with the button first?
 	//plays sound at some letter(buttonpressed.)
 }
 
+//audio Sounds
+var crash = new Audio('sounds/crash.mp3');
+var kickBass = new Audio('sounds/kick-bass.mp3');
+var snare = new Audio('sounds/snare.mp3');
+var tom1 = new Audio('sounds/tom-1.mp3');
+var tom2 = new Audio('sounds/tom-2.mp3');
+var tom3 = new Audio('sounds/tom-3.mp3');
+var tom4 = new Audio('sounds/tom-4.mp3');
